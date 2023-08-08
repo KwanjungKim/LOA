e716fdce630cabf213387eb1476e56f934afe223
hongsi <krs1994@naver.com>
2023-08-08 19:09:34 +0900

    fetch

--- client/src/components/js/Money_Cal.js
+++ client/src/components/js/Money_Cal.js
@@ -1,9 +1,20 @@
 import '../css/Money_Cal.css';
-
+import { useState } from 'react';
 
 
 const Money_Calculator =()=>{
+    const [inputText, setInputText] = useState("");
 
+    const handleFormSubmit =(e)=>{
+        const formData = new FormData();
+        formData.append('inputText', inputText);
+
+        fetch("/주소",{
+            method : "post",
+            cache : "no-cache",
+            body : formData,
+        })
+    }
 
     return(
         <>
@@ -14,8 +25,10 @@ const Money_Calculator =()=>{
                         
                         <div>
                         <h3 className='Money_Cal_name'>경매 계산기</h3>
-                        <input className='Money_Cal_input' placeholder='입력'/>
-                        <button>검색</button>
+                        <form className="Money_Cal_submit" onSubmit={handleFormSubmit} >
+                        <input className='Money_Cal_input' type="text" placeholder='입력' onChange={event => {setInputText(event.target.value)}}/>
+                        <button className='Money_Cal_input' type="submit">검색</button>
+                        </form>
                         </div>
 
                     </div>
